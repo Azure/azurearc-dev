5ea6e7580bba87d1bb8749bc9c89f682d2ec59dc
Zhenyu Huang <zhenyuhuang@microsoft.com>
2023-06-14 11:36:49 +0800

    101 first draft

---
+++ src/sample/101/src/Program.cs
@@ -0,0 +1,19 @@
+﻿// Hello World! program
+using System;
+
+namespace HelloWorld
+{
+    class Hello
+    {
+        static void Main(string[] args)
+        {
+            var counter = 0;
+            var max = 100;
+            while (max is -1 || counter < max)
+            {
+                Console.WriteLine($"Counter: {++counter}");
+                System.Threading.Thread.Sleep(1000);
+            }
+        }
+    }
+}
